a0ae693337d521dfe590654370e45750f7ea1d63
Armen Sargsyan <armen3265@gmail.com>
2020-11-16 18:18:20 +0400

    Added spaces before and after >, *, - and other operators

--- factorial.cpp
+++ factorial.cpp
@@ -3,8 +3,8 @@
 
 int factorial(int n)
 {
-	if(n>1){
-		return n*factorial(n-1);
+	if(n > 1){
+		return n * factorial(n - 1);
 	}
 	else
 	{
--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 #include "factorial.h"
 
 int main(){
-	std::cout<<"What number factorial do you want to count?: ";
+	std::cout << "What number factorial do you want to count?: ";
 	int n;
-	std::cin>>n;
-	int result =  factorial(n);
 	std::cout << "The factorial of " << n << "is equal " << result;
 }
