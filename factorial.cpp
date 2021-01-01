fc50aa2b8185fa0049baf79b620328fa8d198c54
armen3265 <74138554+armen3265@users.noreply.github.com>
2021-01-02 00:41:44 +0400

    Update factorial.cpp
    add runtime_exception

--- factorial.cpp
+++ factorial.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
 #include "factorial.h"
 
 int factorial(int n)
 {
-	if(n > 1){
-		return n * factorial(n - 1);
-	}
-	return 1;
+  if(n < 0)
+  {
+    throw std::runtime_error("n is a smaller of 0");
+  }
+  if(n > 1)
+  {
+    return n * factorial(n-1);
+  }
+  return 1;
 }
